345c585bb3c40869e110c736212a1cd39c9e1cf0
Armaan Arora <armaanarora880@gmail.com>
2026-02-06 23:11:38 -0500

    Added two text fields

--- src/main/java/org/example/csc311_assignment01_gettingstarted/AppController.java
+++ src/main/java/org/example/csc311_assignment01_gettingstarted/AppController.java
@@ -8,18 +8,18 @@ public class AppController {
     private Label welcomeText;
 
     // ToDo 01: add two text fields to the GUI so that the user can find the sum
+    private Label num1;
+    private Label num2;
 
     // ToDo 02: when the button is clicked show the output on the screen
-
     // ToDo 03: you should accept only the numeric values
-
+    @FXML
+    protected void onCalculateButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
     // ToDo 04: modify the CSS file to make the background of the application Lightblue and the button color red
 
     // ToDo 05: commit changes and push back to the same repo
 
 
-    @FXML
-    protected void onCalculateButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
 }
