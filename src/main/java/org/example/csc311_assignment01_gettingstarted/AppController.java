37b81679755a19ab8c2998bce849093674d07424
Armaan Arora <armaanarora880@gmail.com>
2026-02-06 23:23:06 -0500

    Made the click button and to only accept numbers

--- src/main/java/org/example/csc311_assignment01_gettingstarted/AppController.java
+++ src/main/java/org/example/csc311_assignment01_gettingstarted/AppController.java
@@ -15,7 +15,17 @@ public class AppController {
     // ToDo 03: you should accept only the numeric values
     @FXML
     protected void onCalculateButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        String x = num1.getText();
+        String y = num2.getText();
+
+        try{
+            double n1 = Double.parseDouble(x);
+            double n2 = Double.parseDouble(y);
+            welcomeText.setText("Sum is: "+(n1+n2));
+        }
+        catch (Exception e){
+            welcomeText.setText("Invalid Input");
+        }
     }
     // ToDo 04: modify the CSS file to make the background of the application Lightblue and the button color red
 
